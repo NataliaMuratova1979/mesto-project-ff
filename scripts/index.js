6171132d8c89cbe22b987be84b1900eca4b17840
Natalia Muratova <mno@mail.ru>
2024-05-03 08:47:39 +0300

    Сдача проекта номер 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,36 @@
-// @todo: Темплейт карточки
+function makeCard(someObject, someFunction) { 
+    //при вызове функции на место someObject встанет объект массива, на место someFunction - функция deleteCard
 
-// @todo: DOM узлы
-
-const placesList = document.querySelector('.places__list');
-console.log(placesList); //
-
-// @todo: Функция создания карточки
-
-// @todo: Вывести карточки на страницу
-
-//function addCard (nameValue, linkValue)
-
-function makeCard(addCard, deleteCard) {
-
-for (i = 0; i < initialCards.length; i++) {
-    const cardTemplate = document.querySelector('#card-template').content;
+    const cardTemplate = document.querySelector('#card-template').content; // берем шаблон
     console.log(cardTemplate); //ok
+
+    const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true); // клонируем его, создаем объект
+    console.log(placesItem);
+
+    const placesList = document.querySelector('.places__list');
+
+    placesItem.querySelector('.card__title').textContent = someObject.name; //элемент массива 
+    placesItem.querySelector('.card__image').src = someObject.link; //элемент массива
+    placesItem.querySelector('.card__image').alt = someObject.name; //элемент массива
+
+    placesList.append(placesItem); //добавляем карточку 
+
+    const deleteButton = placesItem.querySelector('.card__delete-button'); // назначаем кнопку удаления
     
-    const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
-    console.log(placesItem); //ok
-
-   const linkValue = initialCards[i].link;
-   const nameValue = initialCards[i].name;
-
-   console.log(nameValue); //ok
-   console.log(linkValue); //ok  
-
-   placesItem.querySelector('.card__title').textContent = nameValue; //элемент массива
-
-   placesItem.querySelector('.card__image').src = linkValue; //элемент массива
-
-   placesList.append(placesItem); //добавляем карточку 
-
-   const deleteButton = placesItem.querySelector('.card__delete-button');
-   
-   deleteButton.addEventListener('click', function (evt) {
-    placesItem.remove(); //убираем карточку
-   });
-   }
+    deleteButton.addEventListener('click', () => { 
+        someFunction(placesItem); // 
+    }); 
 }
 
-makeCard();
+function deleteCard(item) { //deleteCard будет на месте deleteFunction при объявлении addCard
+   item.remove(); //убираем карточку
+}
+  
+function addCard(someArray) { //при вызове функции на место someArray встанет наш массив initialCards
+  for (i = 0; i < someArray.length; i++) {
+    const someObject = someArray[i];
+    makeCard(someObject, deleteCard);
+  }
+}
+    
+addCard(initialCards);
